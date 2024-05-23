f52ef523c36f1385e6fbce6eadff7a548ab007b7
Aponymous <johnravelagahit59@gmail.com>
2024-05-23 16:50:46 +0800

    adding hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-     <div>Hello</div>
+      <div>Hello</div>
+      <div>hello</div>
     </main>
   );
 }
